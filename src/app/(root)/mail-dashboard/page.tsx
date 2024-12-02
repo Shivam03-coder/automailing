56048169216d85281286b45c8ad23c551b25c2d8
shivam anand <shivam850anand@gmail.com>
2024-12-02 22:06:40 +0530

    email cards done

--- src/app/(root)/mail-dashboard/page.tsx
+++ src/app/(root)/mail-dashboard/page.tsx
@@ -1,16 +1,13 @@
+import Inbox from "@/app/_components/_mail-dashboard/inbox";
 import { UserButton } from "@clerk/nextjs";
 
 function MaildashBoard() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="bg-muted/50 aspect-video rounded-xl bg-red-400" />
-        <div className="bg-muted/50 aspect-video rounded-xl bg-blue-500" />
-        <div className="bg-muted/50 aspect-video rounded-xl bg-green-300">
-          <UserButton />
-        </div>
+    <div className="f flex flex-1 flex-col gap-4 p-2 pt-0 lg:flex-row">
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-white md:min-h-min">
+        <Inbox />
       </div>
-      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl bg-yellow-300 md:min-h-min" />
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-green-300 md:min-h-min" />
     </div>
   );
 }
